1a0118b560e4115409e1b8f4aa426d958b388c70
lk-li <81033825+lk-li@users.noreply.github.com>
2022-08-14 17:10:52 +0800

    Delete 小程序.py
    无用程序

--- 房天下/小程序.py
+++
@@ -1,3 +0,0 @@
-url = "&calltime=2022-03-15 18:07:41&city=北京&key=xfactivityapi"
-
-# "appname=fangx&city=北京&fromPage=esf&micalltime=20220315181458&miniplat=weixin&misource=101&miversion=p&needCount=1&needRegion=1&scene=1001&v=1.3.44f0c6725d917f8fe4e2791921187fd7d"
