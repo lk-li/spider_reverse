ba2734e2b9bf5f9d09036985f982e04b273f34ab
不愿 <1119035003@qq.com>
2022-08-19 13:53:40 +0800

    企查查web搜索接口1.0

--- qcc/qcc.py
+++ qcc/qcc.py
@@ -1,6 +1,7 @@
 import hashlib
 import hmac
 import requests
+from urllib import parse
 
 
 def jm_sha512(key, value):
@@ -20,28 +21,8 @@ def get_key(api):
     t = e.lower()
     n = t + t
     i = ""
-    codes = {
-        0: "W",
-        1: "l",
-        2: "k",
-        3: "B",
-        4: "Q",
-        5: "g",
-        6: "f",
-        7: "i",
-        8: "i",
-        9: "r",
-        10: "v",
-        11: "6",
-        12: "A",
-        13: "K",
-        14: "N",
-        15: "k",
-        16: "4",
-        17: "L",
-        18: "1",
-        19: "8",
-    }
+    codes = {0: 'W', 1: 'l', 2: 'k', 3: 'B', 4: 'Q', 5: 'g', 6: 'f', 7: 'i', 8: 'i', 9: 'r', 10: 'v', 11: '6', 12: 'A',
+             13: 'K', 14: 'N', 15: 'k', 16: '4', 17: 'L', 18: '1', 19: '8'}
     _n = 20
     for o in range(len(n)):
         r = ord(n[o]) % _n
@@ -50,52 +31,45 @@ def get_key(api):
 
 
 def searchTrademark():
-    dats = '/api/search/searchmind?mindkeywords=true&mindtype=9&pagesize=5&person=true&searchkey=%e4%b8%8a%e6%b5%b7%e5%8c%97%e4%ba%ac&suggest=true'
+    searchKey = parse.quote('万达集团')
+    x_pid = "d61bab86d8195aa1d826eb7b27af85a6"
+    dats = "/api/search/searchmind?mindkeywords=true&mindtype=9&pagesize=5&person=true&searchkey=%s&suggest=true" % (
+        searchKey.lower())
     key = get_key(dats)
-
     value1 = dats + '{}'
-    value2 = dats + "pathString" + "{}" + "2cc2786d4a7a9eaa91f436e78114eaad"
+    value2 = dats + "pathString" + "{}" + x_pid
 
-    sha512_key = jm_sha512(key, value1)
-    sha512_va = jm_sha512(key, value2)
+    sha512_key = jm_sha512(key, value1).lower()
+    sha512_va = jm_sha512(key, value2).lower()
 
-    # 3080dbcecdc9f8a14ebe : c16f4001e9895b0431883db1fb579c06d79f3f87c066315b15c2302b36d0a251c3eee08d5d450b115b13f11015c55f47cab6176426a776a37ba1da4ffca9e847
+    url = "https://www.qcc.com/api/search/searchMind?mindKeyWords=true&mindType=9&pageSize=5&person=true&searchKey=%s&suggest=true" % (
+        searchKey)
+
     headers = {
-        "authority": "www.qcc.com",
         sha512_key[8:28]: sha512_va,
-        "accept": "application/json, text/plain, */*",
-        "accept-language": "zh-CN,zh;q=0.9",
-        "content-type": "application/json",
-        "origin": "https://www.qcc.com",
-        "referer": "https://www.qcc.com/web/search/all?key=%E4%B8%8A%E6%B5%B7",
-        "sec-ch-ua": "\".Not/A)Brand\";v=\"99\", \"Google Chrome\";v=\"103\", \"Chromium\";v=\"103\"",
-        "sec-ch-ua-mobile": "?0",
-        "sec-ch-ua-platform": "\"macOS\"",
-        "sec-fetch-dest": "empty",
-        "sec-fetch-mode": "cors",
-        "sec-fetch-site": "same-origin",
-        "user-agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.0.0 Safari/537.36",
-        "x-pid": "9ed43a8c6134b3092aec23f0b6dee5cd",
-        "x-requested-with": "XMLHttpRequest"
+        'authority': 'www.qcc.com',
+        'accept': 'application/json, text/plain, */*',
+        'accept-language': 'zh-CN,zh;q=0.9',
+        'referer': 'https://www.qcc.com/web/search?key=%E4%B8%87%E8%BE%BE%E9%9B%86%E5%9B%A2',
+        'sec-ch-ua': '".Not/A)Brand";v="99", "Google Chrome";v="103", "Chromium";v="103"',
+        'sec-ch-ua-mobile': '?0',
+        'sec-ch-ua-platform': '"Windows"',
+        'sec-fetch-dest': 'empty',
+        'sec-fetch-mode': 'cors',
+        'sec-fetch-site': 'same-origin',
+        'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.0.0 Safari/537.36',
+        'x-pid': x_pid,
+        'x-requested-with': 'XMLHttpRequest'
     }
-
     cookies = {
-        "QCCSESSID": "9fa13f86d749ca0d6ac0e2780b",
-        "qcc_did": "b29fa4f2-1f16-4cd9-8761-8c7709203175",
-        "UM_distinctid": "1822138d1cdaa5-0f1fb36287b62b-1c525635-1aeaa0-1822138d1ced7e",
-        "_uab_collina": "165841479499709664744316",
-        "CNZZDATA1254842228": "428036109-1658412847-%7C1658420397"
+        "'acw_tc": "3d9f51ab16608858312514469e189c612b877377641dbdedb4d9494e38",
+        "QCCSESSID": "5272c9e1547c79649ad3adb9bb",
+        "qcc_did": "4d932bdf-d347-4604-8669-c15c1461e8a2",
+        "UM_distinctid": "182b4829c3e5a-04737f9112ae08-26021a51-1fa400-182b4829c3f5a5",
+        "CNZZDATA1254842228": "2038214334-1660884693-https%253A%252F%252Fwww.baidu.com%252F%7C1660884693",
+        "_uab_collina": "166088585993231142829859'"
     }
-    url = "https://www.qcc.com/api/search/searchMind"
-    params = {
-        "mindKeyWords": "true",
-        "mindType": "9",
-        "pageSize": "5",
-        "person": "true",
-        "searchKey": "上海北京",
-        "suggest": "true"
-    }
-    response = requests.get(url, headers=headers, cookies=cookies, params=params)
+    response = requests.request("GET", url, headers=headers,cookies=cookies)
 
     print(response.text)
     print(response)
