e6735e90104c1de35a9be04aa9149359a912f49a
不愿 <1119035003@qq.com>
2023-04-14 10:41:57 +0800

    DES_CBC_PKCS5PADding两种方式

--- algorithm_py/DES_CBC_PKCS5Padding.py
+++ algorithm_py/DES_CBC_PKCS5Padding.py
@@ -1,10 +1,58 @@
 from pyDes import des, CBC, PAD_PKCS5
-import binascii
+import binascii,base64
+from Crypto.Cipher import DES3
 """
 加密模式：DES/CBC/pkcs5padding
 需pip先安装依赖包
 > pip install pyDes
 """
+def des3encode(text, key=b"@xi'an%lvdian#xitongbu~&", iv=b'01234567'):
+    # desede/CBC/PKCS5Padding加密
+    # PKCS5Padding
+    # 字符串长度需要是8的倍数
+    BS = 8
+    pad = lambda s: s + (BS - len(s.encode()) % BS) * chr(BS - len(s.encode()) % BS)
+    unpad = lambda s: s[0:-ord(s[-1])]
+
+    # text也需要encode成bytearray
+    plaintext = pad(text).encode()
+
+    # 使用MODE_CBC创建cipher
+    cipher = DES3.new(key, DES3.MODE_CBC, iv)
+
+    # 加密
+    result = cipher.encrypt(plaintext)
+
+    # base64 encode
+    result = (base64.b64encode(result)).decode()
+
+    return result
+
+
+def des3decode(text, key=b"@xi'an%lvdian#xitongbu~&", iv=b'01234567'):
+    # desede/CBC/PKCS5Padding解密
+    # PKCS5Padding
+    # 字符串长度需要是8的倍数
+    BS = 8
+    pad = lambda s: s + (BS - len(s) % BS) * chr(BS - len(s) % BS)
+    unpad = lambda s: s[0:-ord(s[-1])]
+
+    # text也需要encode成bytearray
+    # plaintext = pad(text).encode()
+
+    # 使用MODE_CBC创建cipher
+    cipher = DES3.new(key, DES3.MODE_CBC, iv)
+
+    # base64 decode
+    plain_base64 = base64.b64decode(text)
+
+    # 解密
+    decrypt_text = cipher.decrypt(plain_base64)
+
+    # 截取
+    result = unpad(decrypt_text.decode("utf-8"))
+
+    return result
 
 
 def des_encrypt(s, key='yoyo1234'):
